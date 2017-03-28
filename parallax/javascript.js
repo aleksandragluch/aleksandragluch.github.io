2604ead6d5230b110acd947e9e2817233ddc395e
aleksandragluch <aleksandragluch@interia.pl>
2017-03-28 17:06:47 +0200

    Add files via upload

---
+++ parallax/javascript.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("container").style.marginLeft="250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("container").style.marginLeft="0";
+}
