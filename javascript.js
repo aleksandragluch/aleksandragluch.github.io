d280de7015a2d15da3141dd71699a76b21b1ed38
aleksandragluch <aleksandragluch@interia.pl>
2017-03-28 16:59:55 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,8 +1,9 @@
-function myMap() {
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.2),
-        zoom: 10
-    };
-    var map = new google.maps.Map(mapCanvas, mapOptions);
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+	document.getElementById("container").style.marginLeft="250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("container").style.marginLeft="0";
 }
